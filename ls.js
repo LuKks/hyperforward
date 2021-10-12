5c3413643af009631cb275f1fce4fbad4f690085
Lucas Barrena <lucasbarrena98@gmail.com>
2021-10-12 10:49:32 -0300

    fix ls

--- ls.js
+++ ls.js
@@ -2,34 +2,37 @@ const fs = require('fs');
 const argv = require('minimist')(process.argv.slice(2));
 const homedir = require('os').homedir();
 
-let name = (argv._[1] || '').trim();
-if (!name.length) {
-  throw new Error('Name is required');
-}
-if (name.length > 21) {
-  throw new Error('Name max length must be 21');
-}
-
 let files = getFiles(homedir + '/.ssh/');
-let publicKeys = files.filter(file => file.startsWith('noise_') === 0 && file.endsWith('.pub'));
+let publicKeys = files.filter(file => file.startsWith('noise_') && file.endsWith('.pub'));
 let secretKeys = files.filter(file => file.startsWith('noise_') === 0 && !file.endsWith('.pub'));
 
 console.log('List of my pair keys: (only prints public keys)');
+let myPairKeys = false;
 for (let publicKey of publicKeys) {
-  let secretKey = publicKey.substr(0, publicKey.length - 4);
-  let hasSecretKey = secretKeys.indexOf(secretKey) > -1;
+  let name = publicKey.substr(0, publicKey.length - 4);
+  let hasSecretKey = secretKeys.indexOf(name) > -1;
   if (hasSecretKey) {
-    console.log(publicKey);
+    myPairKeys = true;
+    console.log(name);
   }
 }
+if (!myPairKeys) {
+  console.log('Empty');
+}
+console.log();
 
 console.log('List of known peers:');
+let knownPeers = false;
 for (let publicKey of publicKeys) {
-  let secretKey = publicKey.substr(0, publicKey.length - 4);
-  let hasSecretKey = secretKeys.indexOf(secretKey) > -1;
+  let name = publicKey.substr(0, publicKey.length - 4);
+  let hasSecretKey = secretKeys.indexOf(name) > -1;
   if (!hasSecretKey) {
-    console.log(publicKey);
+    knownPeers = true;
+    console.log(name);
   }
+}
+if (!knownPeers) {
+  console.log('Empty');
 }
 
 function getFiles (source) {
