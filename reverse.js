4af30c464ef0e2dc8b7ebafbf2d3863eca19b459
Lucas Barrena <lucasbarrena98@gmail.com>
2021-10-12 04:01:34 -0300

    clean: reverse

--- reverse.js
+++ reverse.js
@@ -1,56 +1,58 @@
 const noise = require('noise-network');
 const net = require('net');
-const pump = require('pump');
 const fs = require('fs');
 const argv = require('minimist')(process.argv.slice(2));
 const homedir = require('os').homedir();
 
-console.log('argv', argv); // argv { _: [], R: '127.0.0.1:3000', clients: 'crst' }
+argv.keys = (argv.keys || '').trim();
+argv.keys = 'noise' + (argv.keys ? '_' + argv.keys : ''); // => "noise_lks"
 
-let keys = (argv.keys || '').trim();
-keys = 'noise' + (keys ? '_' + keys : '');
-console.log('keys', keys);
+argv.clients = (argv.clients || '').trim().split(','); // => [ 'lks2' ]
 
-let clients = (argv.clients || '').trim().split(',');
-console.log('clients', clients);
-
-let reverse = (argv.R || '').trim().split(':');
+argv.R = (argv.R || '').trim().split(':');
+// valid ports: [tcp: 1-65535] [udp: 0-65535 (optional)]
+// + should support udp port with --udp
+argv.R[1] = parseInt(argv.R[1]);
+if (!argv.R[0] || !argv.R[1]) {
+  throw new Error('-R is invalid (address:port)');
+}
+if (argv.R[1] < 1 || argv.R[1] > 65535) {
+  throw new Error('-R port is invalid (1-65535)');
+}
 
 const serverKeys = {
-  publicKey: Buffer.from(fs.readFileSync(homedir + '/.ssh/' + keys + '.pub', 'utf8'), 'hex'),
-  secretKey: Buffer.from(fs.readFileSync(homedir + '/.ssh/' + keys, 'utf8'), 'hex')
+  publicKey: fs.readFileSync(homedir + '/.ssh/' + argv.keys + '.pub', 'utf8').trim(),
+  secretKey: fs.readFileSync(homedir + '/.ssh/' + argv.keys, 'utf8').trim()
 };
-const clientPublicKeys = clients.map(client => {
-  if (client.length > 21) {
-    return Buffer.from(client, 'hex');
-  } else {
-    return Buffer.from(fs.readFileSync(homedir + '/.ssh/noise_' + client + '.pub', 'utf8'), 'hex');
+const clientPublicKeys = argv.clients.map(client => {
+  // support names
+  if (client.length <= 21) {
+    return fs.readFileSync(homedir + '/.ssh/noise_' + client + '.pub', 'utf8').trim();
   }
+  return client;
 });
 
-console.log('serverKeys.publicKey', serverKeys.publicKey);
-console.log('clientPublicKeys', clientPublicKeys);
-
-// [setting for "servers" in local networks with firewall]
-// allow IN traffic from all interfaces on localAddress:49737
-// 192.168.0.2 49737 ALLOW IN Anywhere
 const server = noise.createServer({
   // authentication
   validate: function (remoteKey, done) {
-    // console.log('server validate', remoteKey.toString('hex'));
     for (let i = 0; i < clientPublicKeys.length; i++) {
       let publicKey = clientPublicKeys[i];
-      if (remoteKey.equals(publicKey)) return done();
+      if (remoteKey.toString('hex') === publicKey) {
+        console.log(Date.now(), 'server validate, allowed public key:\n', remoteKey.toString('hex'));
+        return done();
+      }
     }
+    console.log(Date.now(), 'server validate, denied public key:\n', remoteKey.toString('hex'));
     return done(new Error('Unauthorized key'));
   }
 });
 
+server.on('error', (err) => console.error(err));
+
 server.on('connection', function (client) {
-  // console.log(Date.now(), 'connection', connection.remoteAddress, connection.remotePort, connection.remoteFamily, 'type', info.type, 'client', info.client, 'info peer', info.peer ? [info.peer.host, info.peer.port, 'local?', info.peer.local] : info.peer);
   console.log(Date.now(), 'client connection');
 
-  client.on('error', (err) => console.log(Date.now(), 'client error', err));
+  /*client.on('error', (err) => console.log(Date.now(), 'client error', err));
   client.on('connect', () => console.log(Date.now(), 'client connect'));
   client.on('handshake', () => console.log(Date.now(), 'client handshake'));
   client.on('connected', () => console.log(Date.now(), 'client connected'));
@@ -58,25 +60,29 @@ server.on('connection', function (client) {
   client.on('end', () => console.log(Date.now(), 'client ended'));
   client.on('drain', () => console.log(Date.now(), 'client drained'));
   client.on('finish', () => console.log(Date.now(), 'client finished'));
-  client.on('close', () => console.log(Date.now(), 'client closed'));
+  client.on('close', () => console.log(Date.now(), 'client closed'));*/
 
-  let reversed = net.connect(reverse[1], reverse[0]/*, { allowHalfOpen: true }*/);
-  reversed.on('error', (err) => console.log(Date.now(), 'reversed error', err));
+  // + should support udp connect with --udp
+  let reversed = net.connect(argv.R[1], argv.R[0]);
+  /*reversed.on('error', (err) => console.log(Date.now(), 'reversed error', err));
   reversed.on('timeout', () => console.log(Date.now(), 'reversed timeout'));
   reversed.on('end', () => console.log(Date.now(), 'reversed ended'));
   reversed.on('drain', () => console.log(Date.now(), 'reversed drained'));
   reversed.on('finish', () => console.log(Date.now(), 'reversed finished'));
-  reversed.on('close', () => console.log(Date.now(), 'reversed closed'));
+  reversed.on('close', () => console.log(Date.now(), 'reversed closed'));*/
 
   // handle errors
   client.on('error', reversed.destroy);
   reversed.on('error', client.destroy);
 
   // automatic "end and destroy" after server.close()
-  let clientEnd = () => client.end();
+  let clientEnd = () => {
+    client.end();
+  };
   server.once('$closing', clientEnd);
   client.once('close', () => server.off('$closing', clientEnd));
 
+  // mimic remote to local
   client.on('data', (chunk) => reversed.write(chunk));
   client.on('end', () => reversed.end());
   client.on('finish', () => {
@@ -86,6 +92,7 @@ server.on('connection', function (client) {
   });
   client.on('close', () => reversed.destroy());
 
+  // mimic local to remote
   reversed.on('data', (chunk) => client.write(chunk));
   reversed.on('end', () => client.end());
   reversed.on('finish', () => {
@@ -93,35 +100,31 @@ server.on('connection', function (client) {
     client.end();
   });
   reversed.on('close', () => client.destroy());
+});
 
-  // + will not allow reconnect (default behaviour)?
-  // client.on('close', () => server.close());
-  // reversed.on('close', () => server.close());
+server.on('close', () => {
+  console.log('server closed');
+});
+
+server.on('error', (err) => {
+  console.log('server error', err);
 });
 
 server.listen(serverKeys, function () {
-  console.log('Server is listening on:', server.publicKey.toString('hex'), 'alias:', server.alias);
+  console.log('Server is listening on:\n' + server.publicKey.toString('hex'));
 });
 
-function serverClose (server) {
-  console.log(Date.now(), 'server.end()');
-  server.close(); // stop accepting new connections
-
-  console.log(Date.now(), 'emit $closing');
-  server.emit('$closing');
-}
+// + what happens if Remote lost internet?
+// + --reconnect
 
 process.once('SIGINT', function () {
   console.log(Date.now(), 'SIGINT');
 
-  server.once('close', () => {
-    console.log('server closed');
-    process.exit();
-  });
-  serverClose(server);
+  // just in case event loop somehow is not empty
+  setTimeout(() => process.exit(), 1000);
+  server.once('close', () => process.exit());
 
-  setTimeout(() => {
-    console.log(Date.now(), 'force exit');
-    process.exit();
-  }, 2000);
+  server.server.discovery.destroy({ force: true }); // fix: fast server close
+  server.close();
+  server.emit('$closing');
 });
