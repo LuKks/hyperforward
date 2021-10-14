6256e3a9776d4ef4e30744898eedb75227185a02
Lucas Barrena <lucasbarrena98@gmail.com>
2021-10-14 07:08:10 -0300

    Update index.js

--- index.js
+++ index.js
@@ -80,8 +80,8 @@ function Remote (keyPair, remoteAddress, peers, cb) {
     let remote = ConnectTCP(remoteAddress.address, remoteAddress.port);
     endAfterServerClose(peer, server);
 
-    mimic(remote, peer, { reuse: true }); // replicate remote actions to -> peer
-    mimic(peer, remote); // replicate peer actions to -> remote
+    mimic(remote, peer); // replicate remote actions to -> peer
+    mimic(peer, remote, { reuse: true }); // replicate peer actions to -> remote
   });
 
   return server;
