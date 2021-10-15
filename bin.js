2f4f9889e84b119af9169cfb8abc5a1552664d7a
Lucas Barrena <lucasbarrena98@gmail.com>
2021-10-15 20:25:24 -0300

    changed file from reverse.js to remote.js

--- bin.js
+++ bin.js
@@ -3,7 +3,7 @@ const argv = require('minimist')(process.argv.slice(2));
 
 let command = argv._[0];
 if (argv.R && argv.peers) {
-  command = 'reverse';
+  command = 'remote';
 } else if (argv.L && argv.connect) {
   command = 'local';
 } else if (argv.D && argv.peers) {
