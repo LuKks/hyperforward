dc19460945d798b61502f03c54030abd1427d29d
Lucas Barrena <lucasbarrena98@gmail.com>
2021-10-26 07:15:37 -0300

    +

--- simple-client.js
+++ simple-client.js
@@ -1,62 +1,64 @@
-const DHT = require('@hyperswarm/dht');
-const net = require('net');
-const fetch = require('node-fetch');
-const pump = require('pump');
+const DHT = require('@hyperswarm/dht')
+const net = require('net')
+const fetch = require('node-fetch')
+const pump = require('pump')
 
-const serverKeyPair = DHT.keyPair(Buffer.from('524ad00b147e1709e7fd99e2820f8258fd30ed043c631233ac35e17f9ec10333', 'hex'));
-const clientKeyPair = DHT.keyPair(Buffer.from('c7f7b6cc2cd1869a4b8628deb49efc992109c9fbdfa55ab1cfa528117fff9acd', 'hex'));
+const serverKeyPair = DHT.keyPair(Buffer.from('524ad00b147e1709e7fd99e2820f8258fd30ed043c631233ac35e17f9ec10333', 'hex'))
+const clientKeyPair = DHT.keyPair(Buffer.from('c7f7b6cc2cd1869a4b8628deb49efc992109c9fbdfa55ab1cfa528117fff9acd', 'hex'))
 
-// setup
-(async () => {
-  const localForward = { address: '127.0.0.1', port: '3001' };
+setup()
 
-  const onConnection = await startClientDht({ localForward });
-  await startLocal(onConnection, { localForward });
+async function setup () {
+  const localForward = { address: '127.0.0.1', port: '3001' }
 
-  simulateRequest(localForward);
+  const onConnection = await startClientDht({ localForward })
+  await startLocal(onConnection, { localForward })
+
+  simulateRequest(localForward)
   while (true) {
-    await sleep(5000);
-    await simulateRequest(localForward);
+    await sleep(5000)
+    await simulateRequest(localForward)
   }
-})();
+}
 
 // client
 async function startClientDht ({ localForward }) {
   const node = new DHT({
-    ephemeral: false
-  });
-  await node.ready();
-  console.log('node ready', node.address());
+    ephemeral: false,
+    keyPair: clientKeyPair
+  })
+  await node.ready()
+  console.log('node ready', node.address())
 
   return function (local) {
-    console.log('local connection');
+    console.log('local connection')
 
-    let started = Date.now();
-    let peer = node.connect(serverKeyPair.publicKey);
+    const started = Date.now()
+    const peer = node.connect(serverKeyPair.publicKey)
     peer.on('open', function () {
-      console.log('peer', peer.rawStream.remoteAddress + ':' + peer.rawStream.remotePort, '(' + peer.rawStream.remoteFamily + ')', 'delay', Date.now() - started);
+      console.log('peer', peer.rawStream.remoteAddress + ':' + peer.rawStream.remotePort, '(' + peer.rawStream.remoteFamily + ')', 'delay', Date.now() - started)
 
-      pump(peer, local, peer);
-    });
+      pump(peer, local, peer)
+    })
   }
 }
 
 // local
 function startLocal (onConnection, { localForward }) {
   return new Promise(resolve => {
-    const tcp = net.createServer();
-    tcp.on('close', () => console.log('tcp server closed'));
-    tcp.on('connection', onConnection);
-    tcp.listen(localForward.port, localForward.address, resolve);
-  });
+    const tcp = net.createServer()
+    tcp.on('close', () => console.log('tcp server closed'))
+    tcp.on('connection', onConnection)
+    tcp.listen(localForward.port, localForward.address, resolve)
+  })
 }
 
 async function simulateRequest (localForward) {
-  let response = await fetch('http://' + localForward.address + ':' + localForward.port);
-  let data = await response.text();
-  console.log(data);
+  const response = await fetch('http://' + localForward.address + ':' + localForward.port)
+  const data = await response.text()
+  console.log(data)
 }
 
 function sleep (ms) {
-  return new Promise(resolve => setTimeout(resolve, ms));
+  return new Promise(resolve => setTimeout(resolve, ms))
 }
--- simple-server.js
+++ simple-server.js
@@ -1,54 +1,54 @@
-const DHT = require('@hyperswarm/dht');
-const net = require('net');
-const express = require('express');
-const pump = require('pump');
+const DHT = require('@hyperswarm/dht')
+const net = require('net')
+const express = require('express')
+const pump = require('pump')
 
-const serverKeyPair = DHT.keyPair(Buffer.from('524ad00b147e1709e7fd99e2820f8258fd30ed043c631233ac35e17f9ec10333', 'hex'));
-const clientKeyPair = DHT.keyPair(Buffer.from('c7f7b6cc2cd1869a4b8628deb49efc992109c9fbdfa55ab1cfa528117fff9acd', 'hex'));
+const serverKeyPair = DHT.keyPair(Buffer.from('524ad00b147e1709e7fd99e2820f8258fd30ed043c631233ac35e17f9ec10333', 'hex'))
+const clientKeyPair = DHT.keyPair(Buffer.from('c7f7b6cc2cd1869a4b8628deb49efc992109c9fbdfa55ab1cfa528117fff9acd', 'hex'))
 
-// setup
-(async () => {
-  const remoteForward = { address: '127.0.0.1', port: '3000' };
+setup()
 
-  await startRemote(remoteForward);
+async function setup () {
+  const remoteForward = { address: '127.0.0.1', port: '3000' }
 
-  await startServer({ remoteForward });
-})();
+  await startRemote(remoteForward)
+
+  await startServer({ remoteForward })
+}
 
 // server
 async function startServer ({ remoteForward }) {
   const node = new DHT({
-    ephemeral: false
-  });
-  await node.ready();
-  console.log('node ready', node.address());
+    ephemeral: false,
+    keyPair: serverKeyPair
+  })
+  await node.ready()
+  console.log('node ready', node.address())
 
   const server = node.createServer({
     firewall: function (remotePublicKey, remoteHandshakePayload) {
-      console.log('on firewall, allowed public key:\n' + remotePublicKey.toString('hex'), remoteHandshakePayload);
-      return remotePublicKey.equals(clientKeyPair.publicKey);
+      console.log('on firewall, allowed public key:\n' + remotePublicKey.toString('hex'), remoteHandshakePayload)
+      return remotePublicKey.equals(clientKeyPair.publicKey)
     }
-  });
+  })
 
   server.on('connection', function (peer) {
-    console.log('peer', peer.rawStream.remoteAddress + ':' + peer.rawStream.remotePort, '(' + peer.rawStream.remoteFamily + ')');
+    console.log('peer', peer.rawStream.remoteAddress + ':' + peer.rawStream.remotePort, '(' + peer.rawStream.remoteFamily + ')')
 
-    let remote = net.connect(remoteForward.port, remoteForward.address);
-    pump(peer, remote, peer);
-  });
+    const remote = net.connect(remoteForward.port, remoteForward.address)
+    pump(peer, remote, peer)
+  })
 
-  // this makes the server accept connections on this keypair
-  await server.listen(serverKeyPair);
-  console.log('address after listen', node.address());
-  // console.log('nodes after listen', node.toArray());
+  await server.listen(serverKeyPair)
+  console.log('address after listen', node.address())
 }
 
 // remote (can be anything)
 function startRemote (remoteForward) {
   return new Promise(resolve => {
-    const app = express();
-    app.use((req, res, next) => console.log('req incoming') & next());
-    app.get('/', (req, res) => res.send('Hello World! ' + Math.random()));
-    app.listen(remoteForward.port, remoteForward.address, resolve);
-  });
+    const app = express()
+    app.use((req, res, next) => console.log('req incoming') & next())
+    app.get('/', (req, res) => res.send('Hello World! ' + Math.random()))
+    app.listen(remoteForward.port, remoteForward.address, resolve)
+  })
 }
