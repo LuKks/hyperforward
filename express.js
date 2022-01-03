5d3df67bb1bf90dd13a27e445f247eae3f59de18
Lucas Barrena <lucasbarrena98@gmail.com>
2022-01-03 09:26:56 -0300

    Update express.js

--- express.js
+++ express.js
@@ -3,7 +3,7 @@ const app = express()
 
 app.use(function (req, res, next) {
   console.log('req incoming')
-  req.connection.setTimeout(600 * 1000)
+  // req.connection.setTimeout(600 * 1000)
   next()
 })
 
@@ -11,4 +11,6 @@ app.get('/', function (req, res) {
   res.send('Hello World! ' + Math.random())
 })
 
-app.listen(2999, '0.0.0.0')
+const server = app.listen(2999, '0.0.0.0')
+
+server.keepAliveTimeout = 30000
